ce3457ff60fd12379145df1e0e85fc5b28bb7f47
kysonhensley <hensleykyson@gmail.com>
2026-09-17 11:23:27 -0600

    make changes and finish delay loop lab 3 part2

--- Lab2 Part2.X/nbproject/Makefile-default.mk
+++ Lab2 Part2.X/nbproject/Makefile-default.mk
@@ -95,7 +95,7 @@ ${OBJECTDIR}/Main.o: Main.S  nbproject/Makefile-${CND_CONF}.mk
 	${MP_AS} -mcpu=PIC16F883 -c \
 	-o ${OBJECTDIR}/Main.o \
 	Main.S \
-	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	 -D__DEBUG=1   
 	
 else
 ${OBJECTDIR}/Main.o: Main.S  nbproject/Makefile-${CND_CONF}.mk 
@@ -104,7 +104,7 @@ ${OBJECTDIR}/Main.o: Main.S  nbproject/Makefile-${CND_CONF}.mk
 	${MP_AS} -mcpu=PIC16F883 -c \
 	-o ${OBJECTDIR}/Main.o \
 	Main.S \
-	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -fmax-errors=20 -mwarn=0 -xassembler-with-cpp
+	  
 	
 endif
 
@@ -115,13 +115,13 @@ ${DISTDIR}/Lab2_Part2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbprojec
 	@${MKDIR} ${DISTDIR} 
 	${MP_LD} -mcpu=PIC16F883 ${OBJECTFILES_QUOTED_IF_SPACED} \
 	-o ${DISTDIR}/Lab2_Part2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} \
-	 -D__DEBUG=1   -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -mcallgraph=std -Wl,-Map=${FINAL_IMAGE_NAME_MINUS_EXTENSION}.map -mno-download-hex
+	 -D__DEBUG=1   
 else
 ${DISTDIR}/Lab2_Part2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}: ${OBJECTFILES}  nbproject/Makefile-${CND_CONF}.mk   
 	@${MKDIR} ${DISTDIR} 
 	${MP_LD} -mcpu=PIC16F883 ${OBJECTFILES_QUOTED_IF_SPACED} \
 	-o ${DISTDIR}/Lab2_Part2.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX} \
-	  -mdfp="${DFP_DIR}/xc8"  -msummary=+mem,-psect,-class,-hex,-file,-sha1,-sha256,-xml,-xmlfull -mcallgraph=std -Wl,-Map=${FINAL_IMAGE_NAME_MINUS_EXTENSION}.map -mno-download-hex
+	  
 endif
 
 
